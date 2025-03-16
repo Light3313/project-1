41b4ad351ce1c82012400eebe0e369e3070d64e2
Light3313 <nkuharec73@gmail.com>
2025-03-16 19:24:33 +0000

    form is done

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,31 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Ваш код, який працює з DOM
+    const phoneInput = document.getElementById('phone');
+  
+    phoneInput.addEventListener('input', function (e) {
+      let value = e.target.value.replace(/\D/g, '');
+      if (!value.startsWith('38')) {
+        value = '38' + value;
+      }
+      let formattedValue = '+38';
+  
+      if (value.length > 2) {
+        formattedValue += ' ' + value.slice(2, 5);
+      }
+      if (value.length > 5) {
+        formattedValue += ' ' + value.slice(5, 7);
+      }
+      if (value.length > 7) {
+        formattedValue += ' ' + value.slice(7, 9);
+      }
+      if (value.length > 9) {
+        formattedValue += ' ' + value.slice(9, 12);
+      }
+  
+      e.target.value = formattedValue;
+    });
+  });
